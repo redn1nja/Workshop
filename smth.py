d4d09b799fbc0dce68c150a64833372cec7ae2a3
Dmytro Vasylkiv <dmytro.vasylkiv@gmail.com>
2021-12-15 12:37:30 +0200

    With comments

--- smth.py
+++ smth.py
@@ -27,6 +27,7 @@ def move(ships, table):
             return 'There is not a such coordinate. Maybe You already shot that cell!'
             
 def set_ships(output_list, user_input):
+    #Функція для генерування повного списку позицій, зайнятих кораблями.
     def check_proper(list_of) :
         if list_of[0][0]==list_of[1][0] :
             for i in range(abs(int(list_of[0][1])-int(list_of[1][1]))-1):
@@ -38,10 +39,13 @@ def set_ships(output_list, user_input):
             return list_of
         print("One of your coordinates is not proper. Insert something like 'B1 D1'")
         return set_ships(output_list, input("Type your ship`s beggining and ending point : ").split())
+
+    #Перевірка на правильність вводу.
     checklist=[]
     for i in user_input :
         if i[0] in "ABCDEFGHIJ" and int(i[1]) in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
             checklist.append(True)
+    #Головна частина функції - менеджить її поведінку вцілому.
     if False not in checklist :
         output_list.append([user_input[0], user_input[1]])
         if len(output_list)==10:
